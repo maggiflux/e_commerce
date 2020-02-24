401be1793a12e620c9aefb7abe733423f25150b0
mariela <gila44dri@gmail.com>
2020-02-23 22:27:07 -0300

    desafio e commerce erd gem

---
+++ app/models/product.rb
@@ -0,0 +1,13 @@
+class Product < ApplicationRecord
+  has_and_belongs_to_many :categories
+  has_many :stocks
+
+  def available?()
+    available = false
+    stocks.each do |stock|
+      available = true if stock.quantity > 0  
+      end   
+      return available
+    end
+  end
+
